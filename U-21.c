03317d80750a9a58c1861923ebd8f462f19b64a9
atulsingho <atulsingho457@gmail.com>
2025-12-12 18:26:42 +0600

    Implement program to sum last digits of two numbers

---
+++ U-21.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    
+    long long N, M;
+    scanf("%lld %lld", &N, &M);
+    int last_digit_N = N % 10;
+    int last_digit_M = M % 10;
+    int sum_of_last_digits = last_digit_N + last_digit_M;
+    printf("%d\n", sum_of_last_digits);
+
+    return 0;
+}
